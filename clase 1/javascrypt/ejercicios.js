edb005e092636bb11ee3b6fe610cf0d4824c6190
LucasPedraza <pedrazaalucas@gmail.com>
2024-09-09 22:22:37 -0300

    clase 1

---
+++ clase 1/javascrypt/ejercicios.js
@@ -0,0 +1,44 @@
+/// Ejercicio 1
+
+let nombre = 'Lucas'
+let edad = 32
+let apellido = 'Pedraza'
+
+console.log(nombre)
+
+console.log(edad)
+
+console.log(apellido)
+
+/// Ejercicio 2 
+const ciudad1 = 'buenos aires'
+const ciudad2 = 'cordoba'
+const ciudad3 = 'misiones'
+const ciudad4 = 'mendoza'
+const ciudad5 = 'tucuman'
+
+console.log(ciudad1)
+
+/// Ejercicio 3
+
+let dni = 40187525
+let domicilio = "Centenario 3296"
+
+const carnet = ("Nombre: " + nombre + " " + apellido +" Edad: " + edad + " " +" Documento: "+ dni + " " +" Direccion: " + domicilio + " " +" Provincia: "+ ciudad1)
+console.log(carnet)
+
+/// Ejercicio 4
+
+let persona1 = prompt("Ingrese el nombre de su padre")
+let persona2 = prompt("Ingrese el nombre de su madre")
+let persona3 = prompt("Ingrese el nombre de su hermano")
+let persona4 = prompt("Ingrese el nombre de su hermana")
+let persona5 = prompt("Ingrese el nombre de su primo")
+
+let familia = (persona1 +" "+ persona2 +" "+ persona3 +" "+ persona4 +" "+ persona5)
+alert("Gracias por ingresar "+ familia)
+
+/// Ejercicio 5 descuentos
+
+
+
